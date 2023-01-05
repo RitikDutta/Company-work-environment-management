bd66b66400191b77a570418e60f98c936029a5c9
ritik dutta <ritikduttagd@gmail.com>
2023-01-05 08:30:08 +0530

    flask app sending data via route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
-
+from predictions.predict_landmarks import Prediction
+prediction = Prediction()
+import time
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,8 +13,8 @@ def index():
 @app.route('/process_landmarks', methods=['POST'])
 def process_landmarks():
     landmarks = request.get_json()
-
-    print(pd.DataFrame.from_dict(landmarks).shape)
+    # time.sleep(5)
+    print(prediction.predict2(request.get_json()))
     return jsonify({'status': 'Success'})
 
 if __name__ == '__main__':

cbd5a2476b06d7fc647a279c2e6ea962d1e426c3
ritik dutta <ritikduttagd@gmail.com>
2023-01-20 21:13:18 +0530

    changed error message

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import sys
 
 def data_collection():
 	try:
-		collection = Data_collection(sys.argv[2])
-		collection.pose_collection()
+		collection = Data_collection(sys.argv[2], sys.argv[3])
+		collection.holistic_collection()
 	except IndexError as e:
 		print('main.py has arguments:\nclass name:  name of class to capture data')
 
 def test_prediction():
 	perdiction = LivePredict()
-	perdiction.live_predict()
+	perdiction.live_predict(sys.argv[2])
 
 if __name__ == '__main__':
 	try:
 		globals()[sys.argv[1]]()
 	except KeyError as e:
 		print("An KeyError occurred:", e)
+	except IndexError:
+		print("please enter the operation name")
+		print("data_collection()\ntest_prediction()")

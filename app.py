8246e0e05f59a132c4a182b871863888a961b83f
ritik dutta <ritikduttagd@gmail.com>
2023-02-05 15:40:36 +0530

    changed flask app functions

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
-from predictions.predict_landmarks import Prediction
-prediction = Prediction("pose")
-import time
+from flask import Flask, render_template, Response
+import cv2
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Render the HTML template with the video stream and pose estimation results
     return render_template('index.html')
 
-@app.route('/process_landmarks', methods=['POST'])
-def process_landmarks():
-    landmarks = request.get_json()
-    # time.sleep(5)
-    print(prediction.predict(request.get_json()))
-    return jsonify({'status': 'Success'})
+def gen():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+        else:
+            break
+    cap.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

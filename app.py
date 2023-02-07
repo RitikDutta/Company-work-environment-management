1d0992358a5159d0117988b93dd4530b9b8f801f
ritik dutta <ritikduttagd@gmail.com>
2023-02-07 14:40:50 +0530

    chenged flask for mtcnn predictions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response
 import cv2
+from predictions.live_predict import LivePredict
 
 app = Flask(__name__)
 
@@ -8,21 +9,25 @@ def index():
     return render_template('index.html')
 
 def gen():
-    cap = cv2.VideoCapture(0)
+    live_predict = LivePredict()
     while True:
-        ret, frame = cap.read()
-        if ret:
-            ret, jpeg = cv2.imencode('.jpg', frame)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-        else:
-            break
-    cap.release()
+        live_predict.live_predict_face()
+        processed_image = live_predict.image
+        print(processed_image)
+        ret, jpeg = cv2.imencode('.jpg', processed_image)
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
+    
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(),
+    live_predict = LivePredict(mode="webpage")
+    return Response(live_predict.face_yield(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

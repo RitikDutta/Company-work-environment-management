3458b0e16b66cb31d70a2e3dc8ce670bff7abbc8
ritik dutta <ritikduttagd@gmail.com>
2023-02-13 12:06:26 +0530

    stream results from both class

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+# import cv2
 from flask import Flask, render_template, Response
-import cv2
 from predictions.live_predict import LivePredict
 
 app = Flask(__name__)
@@ -8,26 +8,42 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def gen():
-    live_predict = LivePredict()
-    while True:
-        live_predict.live_predict_face()
-        processed_image = live_predict.image
-        print(processed_image)
-        ret, jpeg = cv2.imencode('.jpg', processed_image)
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+# def gen():
+#     live_predict = LivePredict()
+#     while True:
+#         live_predict.live_predict_face()
+#         processed_image = live_predict.image
+#         print(processed_image)
+#         ret, jpeg = cv2.imencode('.jpg', processed_image)
+#         yield (b'--frame\r\n'
+#                 b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
 
     
 
-@app.route('/video_feed')
-def video_feed():
-    live_predict = LivePredict(mode="webpage")
-    return Response(live_predict.face_yield(),
+# @app.route('/video_feed')
+# def video_feed():
+#     live_predict = LivePredict(mode="webpage")
+#     print("face")
+#     return Response(live_predict.face_yield(),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# @app.route('/video_feed_pose')
+# def video_feed_pose():
+#     live_predict2 = LivePredict(mode="webpage")
+#     print("pose")
+#     return Response(live_predict2.pose_yield(),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed_both')
+def video_feed_both():
+    live_predict3 = LivePredict(mode="webpage")
+    return Response(live_predict3.yield_both(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 
-
 if __name__ == '__main__':
+    
     app.run(debug=True)

7ed6f88d585e473dc53a9ab08a6825014dde0cb5
ritik dutta <ritikduttagd@gmail.com>
2023-04-30 12:23:33 +0530

    fixed cam fronend to backend

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from data_processing.converter import Converter
 from database.database_operations import CassandraCRUD
 import pandas as pd
 # from flask_socketio import SocketIO, emit
-from camera.camera import VideoCamera
+# from camera.camera import VideoCamera
 import time
 import base64
 
@@ -113,38 +113,38 @@ def process_image():
 
 
 
-@app.route('/camera',methods=['POST'])
-def camera():
-    cap=cv2.VideoCapture(0)
-    while True:
-        ret,img=cap.read()
-        img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("static/cam.png",img)
+# @app.route('/camera',methods=['POST'])
+# def camera():
+#     cap=cv2.VideoCapture(0)
+#     while True:
+#         ret,img=cap.read()
+#         img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#         cv2.imwrite("static/cam.png",img)
 
-        # return render_template("camera.html",result=)
-        time.sleep(0.1)
-        return json.dumps({'status': 'OK', 'result': "static/cam.png"})
-        if cv2.waitKey(0) & 0xFF ==ord('q'):
-            break
-    cap.release()
-    # file="/home/ashish/Downloads/THOUGHT.png"
-    # with open(file,'rb') as file:
-    #     image=base64.encodebytes(file.read())
-    #     print(type(image))
-    # return json.dumps({'status': 'OK', 'user': user, 'pass': password});
-    return json.dumps({'status': 'OK', 'result': "static/cam.png"});
+#         # return render_template("camera.html",result=)
+#         time.sleep(0.1)
+#         return json.dumps({'status': 'OK', 'result': "static/cam.png"})
+#         if cv2.waitKey(0) & 0xFF ==ord('q'):
+#             break
+#     cap.release()
+#     # file="/home/ashish/Downloads/THOUGHT.png"
+#     # with open(file,'rb') as file:
+#     #     image=base64.encodebytes(file.read())
+#     #     print(type(image))
+#     # return json.dumps({'status': 'OK', 'user': user, 'pass': password});
+#     return json.dumps({'status': 'OK', 'result': "static/cam.png"});
 
-def gen(camera):
-    while True:
-        data= camera.get_frame()
+# def gen(camera):
+#     while True:
+#         data= camera.get_frame()
 
-        frame=data[0]
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#         frame=data[0]
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 

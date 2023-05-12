8235ec679606fcf063e3f477bcb7fceb7e11ed29
ritik dutta <ritikduttagd@gmail.com>
2023-05-12 17:56:54 +0530

    removed debug mode

--- app.py
+++ app.py
@@ -174,6 +174,7 @@ def process_image2():
 
     prediction = lp.get_both(data['landmarks'], face_image, detection_model=detection_model)
     print(prediction)
+    
     print(data['slider_state'])
     session['my_var_both'] = prediction
     return redirect(url_for('both'))
@@ -206,4 +207,4 @@ def process_train():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)

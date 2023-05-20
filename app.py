d23a62cbb6db3e18f775bfab6a8d3af4a6b34004
ritik dutta <ritikduttagd@gmail.com>
2023-05-21 00:54:12 +0530

    changed port

--- app.py
+++ app.py
@@ -236,5 +236,5 @@ def process_train():
 
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0', port=8080)
     # serve(app, host='0.0.0.0', port=5000, url_scheme='https', ssl_context='adhoc')
